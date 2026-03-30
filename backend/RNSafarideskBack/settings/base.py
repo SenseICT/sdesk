66d7f40a330a3bcac4c106ebc9adc798697bea02
Matt Spurrier <matt@senseict.com>
2026-03-30 17:12:12 +0800

    chore: update frontend port, REST framework config, and Dockerfile build

--- backend/RNSafarideskBack/settings/base.py
+++ backend/RNSafarideskBack/settings/base.py
@@ -104,6 +104,9 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
